f6bbfd577b3bcf4e49e51b3fb3c6b5082aeb0ec8
晓椴 <tdaxei8@gmail.com>
2020-11-28 15:39:22 +0800

    修改自动任务 京喜工厂互助码

--- .github/jd_sharecodes.sh
+++ .github/jd_sharecodes.sh
@@ -8,7 +8,7 @@ PETSHARECODES_ARR=($PETSHARECODES)
 FRUITSHARECODES_ARR=($FRUITSHARECODES)
 PLANT_BEAN_SHARECODES_ARR=($PLANT_BEAN_SHARECODES)
 DDFACTORY_SHARECODES_ARR=($DDFACTORY_SHARECODES)
-JXFACTORY_SHARECODES_ARR=($JXFACTORY_SHARECODES)
+DREAM_FACTORY_SHARE_CODES_ARR=($DREAM_FACTORY_SHARE_CODES)
 
 for sharecode in ${PETSHARECODES_ARR[@]}
     do
@@ -34,7 +34,7 @@ for sharecode in ${DDFACTORY_SHARECODES_ARR[@]}
         #curl 'http://api.turinglabs.net/api/v1/jd/ddfactory/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
     done
 
-for sharecode in ${JXFACTORY_SHARECODES_ARR[@]}
+for sharecode in ${DREAM_FACTORY_SHARE_CODES_ARR[@]}
     do
         echo "JXFACTORY: ${sharecode}"
         #curl 'http://api.turinglabs.net/api/v1/jd/jxfactory/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
