a34d176c6b737e15cfb80060b1a7b0bf1d582ccf
晓椴 <tdaxei8@gmail.com>
2020-12-21 14:56:13 +0800

    互助码

--- .github/jd_sharecodes.sh
+++ .github/jd_sharecodes.sh
@@ -9,7 +9,8 @@ PLANT_BEAN_SHARECODES_ARR=($PLANT_BEAN_SHARECODES)
 DREAM_FACTORY_SHARE_CODES_ARR=($DREAM_FACTORY_SHARE_CODES)
 DDFACTORY_SHARECODES_ARR=($DDFACTORY_SHARECODES)
 JDZZ_SHARECODES_ARR=($JDZZ_SHARECODES)
-echo 'FRUITSHARECODES_ARR:'${FRUITSHARECODES_ARR}'/'
+
+curl 'http://api.turinglabs.net/api/v1/jd/farm/create/15125e19d62d462a95a1f9747aaadbbb/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
 
 IFS=$'\n'
 for sharecode in ${FRUITSHARECODES_ARR[@]}
