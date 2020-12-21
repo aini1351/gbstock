d65b24daec55f8c1e63375e8125258001b19494d
晓椴 <tdaxei8@gmail.com>
2020-12-21 11:24:16 +0800

    互助码

--- .github/jd_sharecodes.sh
+++ .github/jd_sharecodes.sh
@@ -15,7 +15,7 @@ for sharecode in ${FRUITSHARECODES_ARR}
     do
         for sc in ${sharecode[@]}
             do
-                echo 'FRUIT: ''http://api.turinglabs.net/api/v1/jd/farm/create/'${sc}'/'
+                echo 'FRUIT FRUIT: ''http://api.turinglabs.net/api/v1/jd/farm/create/'${sc}'/'
                 #curl 'http://api.turinglabs.net/api/v1/jd/farm/create/'${sc}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
             done
     done
