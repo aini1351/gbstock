9b24a14ca43d45b57fdba25d4c01dabf45b1e668
晓椴 <tdaxei8@gmail.com>
2020-12-21 14:32:28 +0800

    互助码

--- .github/jd_sharecodes.sh
+++ .github/jd_sharecodes.sh
@@ -16,7 +16,7 @@ for sharecode in ${FRUITSHARECODES_ARR}
         IFS="@"
         for sc in ${sharecode[@]}
             do
-                echo 'FRUIT FRUIT FRUIT: ''http://api.turinglabs.net/api/v1/jd/farm/create/'${sc}'/'
+                echo 'FRUIT: http://api.turinglabs.net/api/v1/jd/farm/create/'${sc}'/'
                 #curl 'http://api.turinglabs.net/api/v1/jd/farm/create/'${sc}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
             done
     done
@@ -24,30 +24,54 @@ for sharecode in ${FRUITSHARECODES_ARR}
 IFS=$'\n'
 for sharecode in ${PETSHARECODES_ARR}
     do
-        echo "PET: ${sharecode}"
-        #curl 'http://api.turinglabs.net/api/v1/jd/pet/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+        IFS="@"
+        for sc in ${sharecode[@]}
+            do
+                echo 'PET: http://api.turinglabs.net/api/v1/jd/pet/create/'${sc}'/'
+                #curl 'http://api.turinglabs.net/api/v1/jd/pet/create/'${sc}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+            done
     done
 
+IFS=$'\n'
 for sharecode in ${PLANT_BEAN_SHARECODES_ARR}
     do
-        echo "PLANT_BEAN: ${sharecode}"
-        #curl 'http://api.turinglabs.net/api/v1/jd/bean/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+        IFS="@"
+        for sc in ${sharecode[@]}
+            do
+                echo 'PLANT_BEAN: http://api.turinglabs.net/api/v1/jd/bean/create/'${sc}'/'
+                #curl 'http://api.turinglabs.net/api/v1/jd/bean/create/'${sc}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+            done
     done
 
+IFS=$'\n'
 for sharecode in ${DREAM_FACTORY_SHARE_CODES_ARR}
     do
-        echo "JXFACTORY: ${sharecode}"
-        #curl 'http://api.turinglabs.net/api/v1/jd/jxfactory/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+        IFS="@"
+        for sc in ${sharecode[@]}
+            do
+                echo 'JXFACTORY: http://api.turinglabs.net/api/v1/jd/jxfactory/create/'${sc}'/'
+                #curl 'http://api.turinglabs.net/api/v1/jd/jxfactory/create/'${sc}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+            done
     done
 
+IFS=$'\n'
 for sharecode in ${DDFACTORY_SHARECODES_ARR}
     do
-        echo "DDFACTORY: ${sharecode}"
-        #curl 'http://api.turinglabs.net/api/v1/jd/ddfactory/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+        IFS="@"
+        for sc in ${sharecode[@]}
+            do
+                echo 'DDFACTORY: http://api.turinglabs.net/api/v1/jd/ddfactory/create/'${sc}'/'
+                #curl 'http://api.turinglabs.net/api/v1/jd/ddfactory/create/'${sc}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+            done
     done
 
+IFS=$'\n'
 for sharecode in ${JDZZ_SHARECODES_ARR}
     do
-        echo "JDZZ: ${sharecode}"
-        #curl 'http://code.chiang.fun/api/v1/jd/jdzz/create/'${sharecode}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+        IFS="@"
+        for sc in ${sharecode[@]}
+            do
+                echo 'JDZZ: http://code.chiang.fun/api/v1/jd/jdzz/create/'${sc}'/'
+                #curl 'http://code.chiang.fun/api/v1/jd/jdzz/create/'${sc}'/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2' --compressed -H 'DNT: 1' -H 'Connection: keep-alive'
+            done
     done
