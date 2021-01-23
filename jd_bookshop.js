86988c18da962b94ab7fbcd83875c6aebde626b2
PF <iptver@gmail.com>
2021-01-23 16:46:03 +0800

    update support code

--- jd_bookshop.js
+++ jd_bookshop.js
@@ -30,8 +30,8 @@ ADD_CART = $.isNode() ? (process.env.PURCHASE_SHOPS ? process.env.PURCHASE_SHOPS
 // 加入购物车开关，与东东小窝共享
 
 let inviteCodes = [
-  '28a699ac78d74aa3b31f7103597f8927@2f14ee9c92954cf79829320dd482bf49@fdf827db272543d88dbb51a505c2e869@ce2536153a8742fb9e8754a9a7d361da@38ba4e7ba8074b78851e928af2b4f6b2',
-  '28a699ac78d74aa3b31f7103597f8927@2f14ee9c92954cf79829320dd482bf49@fdf827db272543d88dbb51a505c2e869'
+  '5cea69bc8ae84464bda3b1ee82593eeb@b50cf1a3634c4f99b256e58dea8aad4a@7f3a9ae2e42849a999c2d3cc9306f001',
+  '5cea69bc8ae84464bda3b1ee82593eeb@b50cf1a3634c4f99b256e58dea8aad4a@7f3a9ae2e42849a999c2d3cc9306f001'
 ]
 
 if ($.isNode()) {
