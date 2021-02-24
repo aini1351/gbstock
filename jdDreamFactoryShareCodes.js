0a0ae0b23388776a4ae7035aed61848e62dfe9c4
Funge <iptver@gmail.com>
2021-02-24 09:07:16 +0800

    Update jdDreamFactoryShareCodes.js

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -8,8 +8,9 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let shareCodes = [
-  'V5LkjP4WRyjeCKR9VRwcRX0bBuTz7MEK0-E99EJ7u0k=@Bo-jnVs_m9uBvbRzraXcSA==@-OvElMzqeyeGBWazWYjI1Q==',//账号一的好友shareCode,不同好友中间用@符号隔开
-  '1uzRU5HkaUgvy0AB5Q9VUg==@PDPM257r_KuQhil2Y7koNw==@-OvElMzqeyeGBWazWYjI1Q==',//账号二的好友shareCode，不同好友中间用@符号隔开
+  'nLRg2Blu71e6SsT65ehbzw==@4Hsn6QHhD1kf8SWtwxXoA==',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '4Hsn6QHhD1kf8SWtwxXoA==@Mquwod3b64EirV-hKeLlPw==',//账号二的好友shareCode，不同好友中间用@符号隔开
+  'Mquwod3b64EirV-hKeLlPw==@nLRg2Blu71e6SsT65ehbzw==',
 ]
 // 判断github action里面是否有京喜工厂互助码
 if (process.env.DREAM_FACTORY_SHARE_CODES) {
